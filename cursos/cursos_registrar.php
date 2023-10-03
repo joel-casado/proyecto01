5fbf734e6b9b42c00ef7c9b3668fd8eb73f044f1
JorgeDella <jorge.della@inslapineda.cat>
2023-10-03 19:28:34 +0200

    Inscripcion estudiante

--- cursos/cursos_registrar.php
+++ cursos/cursos_registrar.php
@@ -22,28 +22,41 @@
     //Operaciones de la tabla inscripciones
     $sql = "SELECT * FROM inscripciones";
     $result = mysqli_query($conn, $sql);
+
+    //Calcular el id de la nueva inscripción
+    $newID = mysqli_num_rows($result);
     
-    if ($result['fk_estudiante']==$fk_student && $result['fk_curso']==$fk_curso){
-        echo "Ja et trobes registrat a aquest curs.";
-    }
-
-    //Obtener el id de la nueva inscripcion
-    $fila = COUNT($result);
-    echo 'Número de total de registros: ' . $fila['total'];
-    $ID = $fila['total'] +1;
-
-
-    //Insertar la nueva inscripción en la base de datos
-    $newsql = "INSERT INTO inscripciones (ID_inscripcion, fk_estudiante, fk_curso) VALUES ('$ID', '$fk_student', '$fk_curso')";
-
-    if ($conn->query($newsql) === TRUE) {
-        // Redirigir al alumno luego de inscribir-se
-        echo "Registro exitoso.";
-        header("Location: ../student/home_estudiante.php");
-        exit();
-
+    //Conseguir un array de las inscripciones (chatgpt)
+    if ($result->num_rows > 0) {
+        while($array = $result->fetch_assoc()){
+            if ($array['fk_estudiante'] == $fk_student){
+                if($array['fk_curso'] == $fk_curso){
+                    // Redirigir al alumno
+                    echo "Registro ya existente.";
+                    header("Location: ../student/home_estudiante.php");
+                    exit();
+                }
+                else {
+                    //Insertar la nueva inscripción en la base de datos
+                    $newID++;
+                    $newsql = "INSERT INTO inscripciones (ID_inscripcion, fk_estudiante, fk_curso) VALUES ('$newID', '$fk_student', '$fk_curso')";
+            
+                    if ($conn->query($newsql) === TRUE) {
+                        // Redirigir al alumno luego de inscribir-se
+                        echo "Registro exitoso.";
+                        header("Location: ../student/home_estudiante.php");
+                        exit();
+                    } 
+                    else {
+                        echo "Error en el registro: " . $conn->error;
+                    }
+                }
+            }
+        }
+        //print_r($tabla);
     } else {
-        echo "Error en el registro: " . $conn->error;
+        echo "No se encontraron registros";
     }
-    
+
+
 ?>
